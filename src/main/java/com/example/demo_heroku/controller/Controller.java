52a907be4d06f44b3b2031612fc40df7681a6eb8
xeadmin <xeadmin@DESKTOP-GSE3O5J>
2020-06-30 14:07:58 +0530

    first commit

---
+++ src/main/java/com/example/demo_heroku/controller/Controller.java
@@ -0,0 +1,16 @@
+package com.example.demo_heroku.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	
+	@GetMapping(value="/demo")
+	public String hello()
+	{
+		return "deployement is done:";
+		
+	}
+
+}
